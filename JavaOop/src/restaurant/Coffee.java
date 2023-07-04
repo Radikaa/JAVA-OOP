0e44f82622c4d2776ff1f0a8b4eed97a9a675446
Radoslav Stanchev <stanchev.radoslav89@gmail.com>
2023-07-04 23:02:30 +0300

    up

--- JavaOop/src/restaurant/Coffee.java
+++ JavaOop/src/restaurant/Coffee.java
@@ -3,9 +3,9 @@ package restaurant;
 import java.math.BigDecimal;
 
 public class Coffee extends HotBeverage {
-    public final static double COFFEE_MILLILITERS = 50;
-    public final static BigDecimal COFFEE_PRICE = BigDecimal.valueOf(3.50);
-    public double caffeine;
+    private final static double COFFEE_MILLILITERS = 50;
+    private final static BigDecimal COFFEE_PRICE = BigDecimal.valueOf(3.50);
+    private double caffeine;
 
     public Coffee(String name, double caffeine) {
         super(name, COFFEE_PRICE, COFFEE_MILLILITERS);
--- JavaOop/src/restaurant/Salmon.java
+++ JavaOop/src/restaurant/Salmon.java
@@ -3,7 +3,7 @@ package restaurant;
 import java.math.BigDecimal;
 
 public class Salmon extends MainDish {
-    public final static double SALMON_GRAMS = 22;
+    private final static double SALMON_GRAMS = 22;
 
     public Salmon(String name, BigDecimal price, double grams) {
         super(name, price, SALMON_GRAMS);
