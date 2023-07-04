4111112cf2e8a70bc92d1b00629a07a057a9a0b6
Radoslav Stanchev <stanchev.radoslav89@gmail.com>
2023-07-04 23:21:09 +0300

    up

--- JavaOop/src/person/Citizen.java
+++ JavaOop/src/person/Citizen.java
@@ -1,12 +1,16 @@
 package person;
 
-public class Citizen implements Person {
+public class Citizen implements Person, Identifiable, Birthable {
     private String name;
     private int age;
+    private String birthDate;
+    private String id;
 
-    public Citizen(String name, int age) {
+    public Citizen(String name, int age, String id, String birthDate) {
         this.name = name;
         this.age = age;
+        this.birthDate = birthDate;
+        this.id = id;
     }
 
     @Override
@@ -17,5 +21,15 @@ public class Citizen implements Person {
     @Override
     public int getAge() {
         return age;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return id;
     }
 }
